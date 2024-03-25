5a915d7b3e537a3fa9239de3abd915b5ca54b279
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2024-03-25 17:34:55 +0700

    Update MainActivity.java
    Thiết lập hàm Main để khởi chạy app

--- Cau2_Appnauan/app/src/main/java/ntu/mssv_63134526/MainActivity.java
+++ Cau2_Appnauan/app/src/main/java/ntu/mssv_63134526/MainActivity.java
@@ -9,6 +9,39 @@ import ntu.mssv_63134526.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
-
+    ActivityMainBinding binding;
+    ListAdapter listAdapter;
+    ArrayList<ListData> dataArrayList = new ArrayList<>();
+    ListData listData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        int[] imageList = {R.drawable.mon1, R.drawable.mon2, R.drawable.mon3, R.drawable.mon4, R.drawable.mon5, R.drawable.mon6, R.drawable.mon7};
+        int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients,R.string.cakeIngredients,R.string.pancakeIngredients,R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
+        int[] descList = {R.string.pastaDesc, R.string.maggieDesc, R.string.cakeDesc,R.string.pancakeDesc,R.string.pizzaDesc, R.string.burgerDesc, R.string.friesDesc};
+        String[] nameList = {"Bò bít tết", "Tôm hùm nướng phô mai", "Gà hấp rượu vang", "Cua hoàng đế hấp bia", "Ca thu sốt cà chua","Sườn heo nướng mật ong", "Mực chiên xù"};
+        String[] timeList = {"30 phút", "35 phút", "60 phút","28 phút", "48 phút", "62 phút", "25 phút"};
+        for (int i = 0; i < imageList.length; i++){
+            listData = new ListData(nameList[i], timeList[i], ingredientList[i], descList[i], imageList[i]);
+            dataArrayList.add(listData);
+        }
+        listAdapter = new ListAdapter(MainActivity.this, dataArrayList);
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, DetailedActivity.class);
+                intent.putExtra("name", nameList[i]);
+                intent.putExtra("time", timeList[i]);
+                intent.putExtra("ingredients", ingredientList[i]);
+                intent.putExtra("desc", descList[i]);
+                intent.putExtra("image", imageList[i]);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
