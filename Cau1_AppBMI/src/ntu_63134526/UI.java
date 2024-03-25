ee0aec669a3252c4a2207d162f768128ba22d073
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2024-03-25 13:55:20 +0700

    Thi GK
    Tạo các textFeild để nhập cân nặng và chiều cao

--- Cau1_AppBMI/src/ntu_63134526/UI.java
+++ Cau1_AppBMI/src/ntu_63134526/UI.java
@@ -22,6 +22,8 @@ public class UI extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
+    private JTextField textField;
+    private JTextField textField_1;
     private JLabel lblNewLabel_3; // Thêm biến thành viên cho lblNewLabel_3
 
  
@@ -50,6 +52,26 @@ public class UI extends JFrame {
         panel_1.setBounds(10, 69, 492, 436);
         contentPane.add(panel_1);
         panel_1.setLayout(null);
+
+        JLabel lblNewLabel_1 = new JLabel("Chiều cao(m)");
+        lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblNewLabel_1.setBounds(43, 42, 95, 38);
+        panel_1.add(lblNewLabel_1);
+
+        JLabel lblNewLabel_1_1 = new JLabel("Cân nặng(kg)");
+        lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblNewLabel_1_1.setBounds(43, 114, 95, 38);
+        panel_1.add(lblNewLabel_1_1);
+
+        textField = new JTextField();
+        textField.setBounds(170, 51, 157, 26);
+        panel_1.add(textField);
+        textField.setColumns(10);
+
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+        textField_1.setBounds(173, 123, 154, 26);
+        panel_1.add(textField_1);
         
         // Tạo một nhóm cho các JRadioButton
         ButtonGroup group = new ButtonGroup();
