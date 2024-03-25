b03e80a250891a0487eb9dedc9233fee3da14b52
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2024-03-25 13:47:41 +0700

    Thi GK
    Tạo Panel BMI

--- Cau1_AppBMI/src/ntu_63134526/UI.java
+++ Cau1_AppBMI/src/ntu_63134526/UI.java
@@ -5,38 +5,48 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 
 public class UI extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UI frame = new UI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+ 
 
-	/**
-	 * Create the frame.
-	 */
-	public UI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public UI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 522, 717);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-		setContentPane(contentPane);
-	}
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(0, 128, 255));
+        panel.setBounds(10, 0, 492, 66);
+        contentPane.add(panel);
+        panel.setLayout(null);
 
+        JLabel lblNewLabel = new JLabel("BMI Calculator");
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        lblNewLabel.setBounds(176, 10, 184, 36);
+        panel.add(lblNewLabel);
+        
+        // Tạo một nhóm cho các JRadioButton
+        ButtonGroup group = new ButtonGroup();
+    }
 }
+
+
