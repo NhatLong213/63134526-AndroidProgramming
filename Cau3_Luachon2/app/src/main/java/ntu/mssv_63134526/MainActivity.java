686dddf7ba548acc09f422ce58ae7e30b45c3e74
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2024-03-25 15:26:22 +0700

    Thi GK
    Khởi tạo layout

---
+++ Cau3_Luachon2/app/src/main/java/ntu/mssv_63134526/MainActivity.java
@@ -0,0 +1,23 @@
+package ntu.mssv_63134526;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
